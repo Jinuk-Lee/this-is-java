4182bae5ef1942a6600a4692124003716f8d36ca
jinuklee <20181680@edu.hanbat.ac.kr>
2022-08-12 03:51:10 +0900

    이항 연산자 실습 코드

---
+++ src/chap3/binary_operator/StringEqualsExample.java
@@ -0,0 +1,14 @@
+package chap3.binary_operator;
+public class StringEqualsExample {
+	public static void main(String[] args) {
+		String strVar1 = "�Ź�ö";
+		String strVar2 = "�Ź�ö";
+		String strVar3 = new String("�Ź�ö");
+
+		System.out.println( strVar1 == strVar2);
+		System.out.println( strVar1 == strVar3);
+		System.out.println();
+		System.out.println( strVar1.equals(strVar2));
+		System.out.println( strVar1.equals(strVar3));
+	}
+}
