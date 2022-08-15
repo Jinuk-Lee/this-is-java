e57cabac8b926627f73f8606de0e82c57b96c7a3
jinuklee <20181680@edu.hanbat.ac.kr>
2022-08-16 00:56:27 +0900

    ifelseifelse문 예제 코드

--- src/chap4/IfElseIfElse.java
+++ src/chap4/IfElseIfElse.java
@@ -5,7 +5,7 @@ public class IfElseIfElse {
         int score = 75;
 
         if(score>=90){
-            System.out.println("점수가 100~90"입니다.);
+            System.out.println("점수가 100~90입니다.");
         }
     }
 }
