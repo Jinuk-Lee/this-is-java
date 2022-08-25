e0d0169c58cdef8e6d4b33aa564b693c0e0f8b43
jinuklee <20181680@edu.hanbat.ac.kr>
2022-08-26 01:33:30 +0900

    constructor 사용

---
+++ src/chap6/constructor/declaration/Car.java
@@ -0,0 +1,8 @@
+package chap6.constructor.declaration;
+
+public class Car {
+	//������
+	Car(String color, int cc) {
+	}
+}
+
---
+++ src/chap6/constructor/declaration/CarExample.java
@@ -0,0 +1,8 @@
+package chap6.constructor.declaration;
+
+public class CarExample {
+	public static void main(String[] args) {
+		Car myCar = new Car("����", 3000);
+		//Car myCar = new Car();  (x)
+	}
+}
