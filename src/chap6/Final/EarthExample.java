87e7bf9d73eead01d1891c6922c7af94bc47c662
jinuklee <20181680@edu.hanbat.ac.kr>
2022-08-26 01:31:34 +0900

    final 키워드 사용

---
+++ src/chap6/Final/EarthExample.java
@@ -0,0 +1,9 @@
+package chap6.Final;
+
+public class EarthExample {
+	public static void main(String[] args) {		
+		System.out.println("������ ������: " + Earth.EARTH_RADIUS + " km");
+		System.out.println("������ ǥ����: " + Earth.EARTH_SURFACE_AREA + "  km^2");
+	}
+}
+
---
+++ src/chap6/Final/PersonExample.java
@@ -0,0 +1,16 @@
+package chap6.Final;
+
+public class PersonExample {
+	public static void main(String[] args) {
+		Person p1 = new Person("123456-1234567", "계백");
+		
+		System.out.println(p1.nation);
+		System.out.println(p1.ssn);
+		System.out.println(p1.name);
+		
+		//p1.nation = "usa"; ->final 필드는 수정 불가합니다.
+		//p1.ssn = "654321-7654321";
+		p1.name = "을지문덕";;
+	}
+}
+
