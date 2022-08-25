2434f76628963bfc24d6a895625de4fb5fc3cf73
jinuklee <20181680@edu.hanbat.ac.kr>
2022-08-26 01:34:54 +0900

    어노테이션 예제 코드

---
+++ src/chap6/annotation/Service.java
@@ -0,0 +1,18 @@
+package chap6.annotation;
+
+public class Service {
+	@PrintAnnotation
+	public void method1() {
+		System.out.println("���� ����1");
+	}
+
+	@PrintAnnotation("*")
+	public void method2() {
+		System.out.println("���� ����2");
+	}	
+	
+	@PrintAnnotation(value="#", number=20)
+	public void method3() {
+		System.out.println("���� ����3");
+	}	
+}
