e4c1f18e49bf824ac1ccb42f03e9be579d4baa09
jinuklee <20181680@edu.hanbat.ac.kr>
2022-08-26 01:28:21 +0900

    new 연산자로 객체 생성

---
+++ src/chap6/New/StudentExample.java
@@ -0,0 +1,12 @@
+package chap6.New;
+
+public class StudentExample {
+	public static void main(String[] args) {
+		Student s1 = new Student();
+		System.out.println("s1 ������ Student ��ü�� �����մϴ�.");
+		
+		Student s2 = new Student();
+		System.out.println("s2 ������ �� �ٸ� Student ��ü�� �����մϴ�.");
+	}
+}
+
