568af15f47a721c8b60b171509b989bfb7df9f52
jinuklee <20181680@edu.hanbat.ac.kr>
2022-09-14 15:21:49 +0900

    삭제 코드

--- src/chap4/for/ForPrintFrom1To10Example.java
+++
@@ -1,9 +0,0 @@
-package chap4.For;
-
-public class ForPrintFrom1To10Example {
-    public static void main(String[] args) {
-        for(int i =1;i<=10;i++){
-            System.out.println(i);
-        }
-    }
-}
