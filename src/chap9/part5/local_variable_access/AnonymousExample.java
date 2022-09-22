cf203c213325d996c9694629671957d94148f4d6
jinuklee <20181680@edu.hanbat.ac.kr>
2022-09-23 05:40:54 +0900

    익명 객체의 로컬 변수 사용

---
+++ src/chap9/part5/local_variable_access/AnonymousExample.java
@@ -0,0 +1,8 @@
+package chap9.part5.local_variable_access;
+
+public class AnonymousExample {
+	public static void main(String[] args) {
+		Anonymous anony = new Anonymous();
+		anony.method(0, 0);
+	}
+}
---
+++ src/chap9/part5/local_variable_access/Calculatable.java
@@ -0,0 +1,5 @@
+package chap9.part5.local_variable_access;
+
+public interface Calculatable {
+	public int sum();
+}
