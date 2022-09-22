74207def228c6d490026522b09f723d558b664ef
jinuklee <20181680@edu.hanbat.ac.kr>
2022-09-23 05:40:19 +0900

    익명 구현 객체 생성

---
+++ src/chap9/part5/button_event/Window.java
@@ -0,0 +1,26 @@
+package chap9.part5.button_event;
+
+public class Window {
+	Button button1 = new Button();
+	Button button2 = new Button();
+	
+	//�ʵ� �ʱⰪ���� ����
+	Button.OnClickListener listener = new Button.OnClickListener() {
+		@Override
+		public void onClick() {
+			System.out.println("��ȭ�� �̴ϴ�.");
+		}
+	};
+	
+	Window() {
+		button1.setOnClickListener(listener);
+		
+		//�Ű������� ����
+		button2.setOnClickListener(new Button.OnClickListener() {
+			@Override
+			public void onClick() {
+				System.out.println("�޽����� �����ϴ�.");
+			}
+		});
+	}
+}
