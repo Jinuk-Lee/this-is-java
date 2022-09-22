cca9df7a92748f09d0a8774721c3210e64775cb0
jinuklee <20181680@edu.hanbat.ac.kr>
2022-09-23 03:54:08 +0900

    버튼 이벤트 처리

---
+++ src/chap9/part4/nested_interface/ButtonExample.java
@@ -0,0 +1,13 @@
+package chap9.part4.nested_interface;
+
+public class ButtonExample {
+	public static void main(String[] args) {
+		Button btn = new Button();
+		
+		btn.setOnClickListener(new CallListener());
+		btn.touch();
+		
+		btn.setOnClickListener(new MessageListener());
+		btn.touch();
+	}
+}
