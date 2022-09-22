591eee73a1b916d61aeb7a657bd8dd2516595265
jinuklee <20181680@edu.hanbat.ac.kr>
2022-09-23 05:19:35 +0900

    익명 자식 객체 생성

--- src/chap9/part5/anonymous_extends/Anonymous.java
+++ src/chap9/part5/anonymous_extends/Anonymous.java
@@ -1,27 +1,27 @@
 package chap9.part5.anonymous_extends;
 
 public class Anonymous {
-	//�ʵ� �ʱⰪ���� ����
+	//필드 초기값으로 대입
 	Person field = new Person() {
 		void work() {
-			System.out.println("����մϴ�.");
+			System.out.println("출근합니다.");
 		}
 		@Override
 		void wake() {
-			System.out.println("6�ÿ� �Ͼ�ϴ�.");
+			System.out.println("6시에 일어납니다.");
 			work();
 		}
 	};
 	
 	void method1() {
-		//���ú��������� ����
+		//로컬 변수값으로 대입
 		Person localVar = new Person() {
 			void walk() {
-				System.out.println("��å�մϴ�.");
+				System.out.println("산책합니다.");
 			}
 			@Override
 			void wake() {
-				System.out.println("7�ÿ� �Ͼ�ϴ�.");
+				System.out.println("7시에 일어납니다.");
 				walk();
 			}
 		};
