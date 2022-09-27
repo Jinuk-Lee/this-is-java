293f01e8cd6a245c09bc90b89bb64f94c564a2b6
jinuklee <20181680@edu.hanbat.ac.kr>
2022-09-28 02:14:20 +0900

    어나니머스 객체 코드

--- src/chap9/part5/anonymous_implements/Anonymous.java
+++ src/chap9/part5/anonymous_implements/Anonymous.java
@@ -1,46 +1,33 @@
 package chap9.part5.anonymous_implements;
 
 public class Anonymous {
-	//�ʵ� �ʱⰪ���� ����
-	RemoteControl field = new RemoteControl() {
+	RemoteControl field = new RemoteControl() { // 필드 선언과 초기값 대입
 		@Override
-		public void turnOn() {
-			System.out.println("TV�� �մϴ�.");
+		public void turnOn(){
+			System.out.println("TV를 켭니다.");
 		}
 		@Override
-		public void turnOff() {
-			System.out.println("TV�� ���ϴ�.");
+		public void turnOff(){
+			System.out.println("TV를 끕니다.");
 		}
 	};
-	
-	void method1() {
-		//���ú��������� ����
-		RemoteControl localVar = new RemoteControl() {
+	void method1(){
+		RemoteControl localVar = new RemoteControl() { // 로컬 변수 선언과 초기값 대입
 			@Override
 			public void turnOn() {
-				System.out.println("Audio�� �մϴ�.");
+				System.out.println("오디오를 켭니다.");
 			}
 			@Override
 			public void turnOff() {
-				System.out.println("Audio�� ���ϴ�.");
+				System.out.println("오디오를 끕니다.");
 			}
 		};
-		//���ú��� ���
 		localVar.turnOn();
 	}
-	
-	void method2(RemoteControl rc) {
+	void method2(RemoteControl rc){
 		rc.turnOn();
 	}
 }
-
-
-
-
-
-
-
-
 
 
 
--- src/chap9/part5/anonymous_implements/AnonymousExample.java
+++ src/chap9/part5/anonymous_implements/AnonymousExample.java
@@ -1,24 +1,22 @@
 package chap9.part5.anonymous_implements;
 
 public class AnonymousExample {
-	public static void main(String[] args) {
-		Anonymous anony = new Anonymous();
-		//�͸� ��ü �ʵ� ���
-		anony.field.turnOn();
-		//�͸� ��ü ���ú��� ���
-		anony.method1();
-		//�͸� ��ü �Ű��� ���
-		anony.method2(
-			new RemoteControl() {
-				@Override
-				public void turnOn() {
-					System.out.println("SmartTV�� �մϴ�.");
-				}
-				@Override
-				public void turnOff() {
-					System.out.println("SmartTV�� ���ϴ�.");
-				}
-			}
-		);
-	}
+    public static void main(String[] args) {
+        Anonymous anony = new Anonymous();
+        anony.field.turnOn();
+        anony.method1();
+        anony.method2(
+                new RemoteControl() { // 메소드의 매개값으로 사용
+                    @Override
+                    public void turnOn() {
+                        System.out.println("SmartTV를 켭니다.");
+                    }
+
+                    @Override
+                    public void turnOff() {
+                        System.out.println("SmartTV를 끕니다.");
+                    }
+                }
+        );
+    }
 }
