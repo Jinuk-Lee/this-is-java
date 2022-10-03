ebda00cb8e00179ed201268a3a373dda06c0a5dc
jinuklee <20181680@edu.hanbat.ac.kr>
2022-10-04 01:13:25 +0900

    메인 스레드와 작업 스레드가 동시에 실행

--- src/chap12/part2/createthread/BeepPrintExample3.java
+++ src/chap12/part2/createthread/BeepPrintExample3.java
@@ -16,12 +16,11 @@ public class BeepPrintExample3 {
 				}
 			}
 		};*/
-		
-		
+
 		thread.start();		
 		
 		for(int i=0; i<5; i++) {
-			System.out.println("��");
+			System.out.println("띵");
 			try { Thread.sleep(500); } catch(Exception e) {}
 		}
 	}
