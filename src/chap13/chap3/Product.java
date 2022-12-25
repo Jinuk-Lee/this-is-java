25bd1790f238dea30cd8e6db8215ea68223f5b30
jinuklee <20181680@edu.hanbat.ac.kr>
2022-12-25 14:06:56 +0900

    제네릭 클래스

---
+++ src/chap13/chap3/Product.java
@@ -0,0 +1,22 @@
+package chap13.chap3;
+
+public class Product {
+    private T kind;
+    private M model;
+
+    public T getKind() {
+        return this.kind;
+    }
+
+    public M getModel() {
+        return this.model;
+    }
+
+    public void setKind() {
+        this.kind = kind;
+    }
+
+    public void setModel() {
+        this.model = model;
+    }
+}
